6f194ef5b767476d8b40330ee5842b7350d4208b
playerplayer <bohdan.k2000@gmail.com>
2021-10-03 18:54:47 -0400

    first commit

---
+++ Messenger/MessengerClient/MessengerClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MessengerClient
+//
+//  Created by Dan on 10/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Messenger/MessengerClient/MessengerClient/MessengerClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerClientApp.swift
+//  MessengerClient
+//
+//  Created by Dan on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MessengerClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
